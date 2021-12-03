6286ee304a8aa149ab04f08ee5bdfe09538e852f
старейшина <blakedyblake@gmail.com>
2021-12-03 10:58:46 -0700

    backbone of login

---
+++ src/components/UpperTool.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const UpperTool = ()=>{
+    return(
+        <div className="toolbar">
+            
+        </div>
+    )
+}
+
+export default UpperTool;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import UpperTool from "../components/UpperTool";
+
+const LoginPage = ()=>{
+    return(
+        <>
+            <UpperTool/>
+            <section className="midmain">
+                <h1>Welcome Back!</h1>
+                <h3>Where do you want to begin</h3>
+                <button className="link"></button>
+                <button className="link"></button>
+
+            </section>
+        </>
+    )
+}
+
+export default LoginPage;
