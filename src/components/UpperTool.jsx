6ac551b25cece2967bb6b5b6be290f5272723958
старейшина <blakedyblake@gmail.com>
2021-12-03 15:57:19 -0700

    First Page

--- src/components/UpperTool.jsx
+++ src/components/UpperTool.jsx
@@ -2,10 +2,12 @@ import react from "react";
 
 const UpperTool = ()=>{
     return(
-        <div className="toolbar" style={{backgroundColor:'black', color:'white',display:'flex',flexDirection:'row'}}>
-            <div id='menu'> &#9776;</div>
-            <div id='search'>&#x1F50D;</div>
-            <img src="../icons/account.png" alt='Yo'style={{boxShadow:'10px 10px 10px white'}}/>
+        <div className="toolbar" >
+            <section className='scrunch'>
+                <div className='icon' id='menu'> &#9776;</div>
+                <div className='icon'id='search'>&#x1F50D;</div>
+                <div className='icon'id="account">&#x13020;</div>
+            </section>
         </div>
     )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,16 +3,16 @@ import UpperTool from "../components/UpperTool";
 
 const LoginPage = ()=>{
     return(
-        <>
+        <section className='pages'>
             <UpperTool/>
             <section className="midmain">
                 <h1>Welcome Back!</h1>
-                <h3>Where do you want to begin</h3>
-                <button className="link">View My Memories</button>
+                <h3>Where do you want to begin?</h3>
+                <button className="link">View My Memories</button><br/>
                 <button className="link">Games For Kids</button>
 
             </section>
-        </>
+        </section>
     )
 }
 
